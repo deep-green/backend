fa602d34c00593f07fbbfed89e86b477e4c84b40
Tobias Koppmann <tobias.koppmann@gmail.com>
2018-05-31 17:54:02 +0200

    add test for "makeMove"

--- test/main.test.js
+++ test/main.test.js
@@ -166,4 +166,21 @@ describe('[backend -> client] interface communication', () => {
 
 describe('[client -> backend] interface communication', () => {
 
+    test('backend gets valid "makeMove"', (done) => {
+        let check_data = {
+            "FEN": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            "ID_game": 2,
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"  
+        };
+        
+        socket.on('connect', () => {
+            socket.emit('makeMove', check_data);
+            done();
+        })
+    
+        ioServer.on('makeMove', (data) => {
+            expect(dg_interface.check_makeMove(data)).toBeTruthy;
+        });
+
+    });
 });
