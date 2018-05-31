ef96102b3c6a928473220c36b136181d2dce3894
Tobias Koppmann <tobias.koppmann@gmail.com>
2018-05-31 18:02:13 +0200

    add test for "accept"

--- test/main.test.js
+++ test/main.test.js
@@ -255,6 +255,23 @@ describe('[client -> backend] interface communication', () => {
         });
 
     });
+
+    test('backend gets valid "accept"', (done) => {
+        let check_data = {
+            "ID_game": 2,
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"  
+          }
+        
+        socket.on('connect', () => {
+            socket.emit('accept', check_data);
+            done();
+        })
+    
+        ioServer.on('accept', (data) => {
+            expect(dg_interface.check_newGame(data)).toBeTruthy;
+        });
+
+    });
     
 
 });
